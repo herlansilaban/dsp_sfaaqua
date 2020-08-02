98647ee5d147a0be4126e361b00d45b9db0ea4b6
Herlan Silaban <Herlan76ban@gmail.com>
2020-08-02 15:30:11 +0700

    update

--- index.php
+++ index.php
@@ -4,9 +4,7 @@ $server_key = "SB-Mid-server-9R1BlaJNMqDbwUeln4mij2dh";
 
 $is_production = false;
 
-$api_url = $is_production ? 
-    'https://app.midtrans.com/snap/v1/transactions' : 
-    'https://app.sandbox.midtrans.com/snap/v1/transactions';
+$api_url = $'https://app.sandbox.midtrans.com/snap/v1/transactions';
 
 if( !strpos($_SERVER['REQUEST_URI'], '/charge') ) {
   http_response_code(404); 
@@ -40,7 +38,7 @@ function chargeAPI($api_url, $server_key, $request_body){
     CURLOPT_HTTPHEADER => array(
       'Content-Type: application/json',
       'Accept: application/json',
-      'Authorization: Basic ' . base64_encode($server_key . ':')
+      'Authorization: Basic ' . base64_encode($server_key)
     ),
     CURLOPT_POSTFIELDS => $request_body
   );
